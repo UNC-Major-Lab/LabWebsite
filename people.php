85df9eb3fca1035e77cec2feff6fb523faca9cce
Dennis Goldfarb <dennisg@dhcp-v439-01358.med-1x-wireless.unc.edu>
2014-10-10 11:14:17 -0400

    removed steve from the list of lab members

--- people.php
+++ people.php
@@ -1,233 +1,225 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <?php include "header.html" ?>
-    <title>Major Lab - People</title>
-    <link href="css/js-image-slider.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="css/main.css" />
-    <script src="js/js-image-slider.js" type="text/javascript"></script>
-    <script type="text/javascript" src="js/google-analytics.js"></script>
-  </head>
-  <body>
-    <div id="wrap">
-      <?php include "navbar.html";?>
-      <div id="stripe2">
-	
-	<div id="str2">
-	  <div class="innerdiv">
-	    <div class="pageHeader">
-            <h1>Lab Personnel</h1>
-	    </div>
-	    
-	    <div class="content">
-	      <div class="sliderContainer">
-		<div class="sliderFrame">
-		  <div id="slider" class="slider">
-		    <img src="img/lab/lab1.png" alt="" />
-		    <img src="img/lab/lab4.png" alt="" />
-		    <img src="img/lab/lab3.png" alt="" />
-		    <img src="img/lab/lab5.png" alt="" />
-		    <img src="img/lab/lab2.png" alt="" />
-		  </div>
-		</div>
-	      </div>
-	      
-	      
-	      
-	      <table class="people" width="100%">
-		<tbody>
-		  <tr>
-		    <td>
-		      <!-- Ben Major -->
-		      <div class="personCell">
-			<h3> Principal Investigator </h3>
-			<img class="personImg" alt="Dr. Ben Major" src="img/ben.png"/>
-			<p><strong>Ben Major, PhD <a href="files/ben_biosketch.pdf" target="_blank">(Biosketch)</a></strong></p>
-			<a href="mailto:ben_major@med.unc.edu" target="_blank">ben_major@med.unc.edu</a>
-			<p class="desc"></p>
-		      </div>
-		    </td>
-		    <td>
-		      <!-- Priscila Siesser -->
-		      <div class="personCell">
-			<h3> Research Associate </h3>
-			<img class="personImg" alt="Priscila Siesser" src="img/priscila.jpg"/>
-			<p><strong>Priscila Siesser, PhD</strong></p>
-			<a href="mailto:priscila_siesser@med.unc.edu" target="_blank">priscila_siesser@med.unc.edu</a>
-			<p class="desc">Cell Biology.<br/>Cytoskeleton and motility.</p>
-		      </div>
-		    </td>
-		    <td>
-		      <!-- Matt Walker -->
-		      <div class="personCell">
-			<h3> Postdoctoral Fellow </h3>
-			<img class="personImg" alt="Matt Walker" src="img/matt.png"/>
-			<p><strong>Matt Walker, PhD</strong></p>
-			<a href="mailto:mattwa@med.unc.edu" target="_blank">mattwa@med.unc.edu</a>
-			<p class="desc">Development of functional screening platforms. Team Wnt.</p>
-		      </div>
-		    </td>
-		    <td>
-		      <!-- Feng Yang -->
-		      <div class="personCell">
-			<h3> Postdoctoral Fellow </h3>
-			<img class="personImg" alt="Feng Yang" src="img/feng.png"/>
-			<p><strong>Feng Yang, PhD</strong></p>
-			<a href="mailto:fyan@unc.edu" target="_blank">fyan@unc.edu</a>
-			<p class="desc">Mass spectrometry, activity-based proteomics, and phosphoproteomics</p>
-		      </div>
-		    </td>
-		  </tr>
-		  
-		  <tr>
-		    <td>
-		      <!-- Dennis Goldfarb -->
-		      <div class="personCell">
-			<h3> Graduate Student </h3>
-			<img class="personImg" alt="Dennis Goldfarb" src="img/dennis2.png"/>
-			<p><strong>Dennis Goldfarb</strong></p>
-			<a href="mailto:goldfarb@cs.unc.edu">goldfarb@cs.unc.edu</a>
-			<p class="desc">Co-mentored by <a href="http://www.cs.unc.edu/~weiwang/" target="_blank" style="font-size:12px">Wei Wang</a>
-			</p>
-			<p class="desc">Protein-protein interaction prediction and software development.</p>
-		      </div>
-		    </td>
-		    <td>
-		      <!-- Kathleen Mulvaney -->
-		      <div class="personCell">
-			<h3> Graduate Student </h3>
-			<img class="personImg" alt="Kathleen Mulvaney" src="img/kathleen.png"/>
-			<p><strong>Kathleen Mulvaney</strong></p>
-			<a href="mailto:kmulvane@email.unc.edu" target="_blank">kmulvane@email.unc.edu</a>
-			<p class="desc">Team Keap/Nrf.</br>Lab safety officer.</p>
-		      </div>
-		    </td>
-		    <td>
-		      <!-- Erica Cloer -->
-		      <div class="personCell">
-			<h3> Graduate Student </h3>
-			<img class="personImg" alt="Erica Cloer" src="img/erica.png"/>
-                        <p>(blurry) <strong>Erica Cloer</strong></p>
-			<a href="mailto:erica_cloer@med.unc.edu" target="_blank">erica_cloer@med.unc.edu</a>
-			<p class="desc">Team Keap/Nrf.</p>
-		      </div>
-		    </td>
-                    <td>
-                      <!-- Tigist Y Tamir -->
-                      <div class="personCell">
-                        <h3> Graduate Student </h3>
-                        <img class="personImg" alt="Tigist Y Tamir" src="img/tigist.jpeg"/>
-                        <p><strong>Tigist Y Tamir</strong></p>
-                        <a href="mailto:tytamir@email.unc.edu" target="_blank">tytamir@email.unc.edu</a>
-                        <p class="desc">Team Keap/Nrf.</br>Team Wnt.</p>
-                      </div>
-                    </td>
-		  </tr>
-		  <tr>
-                    <td>
-                      <!-- Abby Winn -->
-                      <div class="personCell">
-                        <h3> Lab Technician </h3>
-                        <img class="personImg" alt="Abby Winn" src="img/Abby.png"/>
-                        <p><strong>Abby Winn</strong></p>
-                        <a href="mailto:arwinn@live.unc.edu" target="_blank">arwinn@live.unc.edu</a>
-                        <p class="desc">Support Team.</br>Minion Master.</p>
-                      </div>
-                    </td>
-                    <td>
-                      <!-- Joe Dayaa -->
-                      <div class="personCell">
-                        <h3> Undergraduate </h3>
-                        <img class="personImg" alt="Joe Dayaa" src="img/johndoe.png"/>
-                        <p><strong>Joe Dayaa</strong></p>
-                        <a href="mailto:dayaa@live.unc.edu" target="_blank">dayaa@live.unc.edu</a>
-                        <p class="desc">Team Wnt.</p>
-                      </div>
-                    </td>
-                    <td>
-                      <!-- Nalini Bernadette Peres-da-silva -->
-                      <div class="personCell">
-                        <h3> Undergraduate </h3>
-                        <img class="personImg" alt="Nalini Bernadette Peres-da-silva" src="img/nalini.png"/>
-                        <p><strong>Nalini Bernadette</br>Peres-da-silva</strong></p>
-                        <a href="mailto:nalinip@live.unc.edu" target="_blank">nalinip@live.unc.edu</a>
-                        <p class="desc">Support Team.</br>Minion.</p>
-                      </div>
-                    </td>
+<head>
+	<?php include "header.html" ?>
+	<title>Major Lab - People</title>
+	<link href="css/js-image-slider.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<script src="js/js-image-slider.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/google-analytics.js"></script>
+</head>
+<body>
+	<div id="wrap">
+		<?php include "navbar.html";?>
+		<div id="stripe2">
+			<div id="str2">
+				<div class="innerdiv">
+					<div class="pageHeader">
+						<h1>Lab Personnel</h1>
+					</div>
 
-                    <td>
-                      <!-- Steve Partin -->
-                      <div class="personCell">
-                        <h3> Admin. Associate</h3>
-                        <img class="personImg" alt="Steve Partin" src="img/johndoe.png"/>
-                        <p><strong>Steve Partin</strong></p>
-                        <a href="mailto:spartin@email.unc.edu" target="_blank">spartin@email.unc.edu</a>
-                        <p class="desc">Administrative Duties.</p>
-                      </div>
-                    </td>
-                    <td/>
-                    <td/>
-                  </tr>
-		</tbody>
-	      </table>
-	      
-	      
-	      <p class="divider-blue">Major Lab Alumni</p>
-	      
-	      <table class="people" style="width:100%; margin-top:15px;">
-		<tbody>
-		  <tr>
-		    <td>
-		      <!-- Kathryn Mackinnon -->
-		      <div class="personCell">
-			<h3> Postdoctoral Fellow </h3>
-			<img class="personImg" alt="Kathryn Mackinnon" src="img/kathryn.png"/>
-			<p><strong>Kathryn Mackinnon, PhD</strong></p>
-			<a href="mailto:kmackinnon@wwmponline.com" target="_blank">kmackinnon@wwmponline.com</a>
-                        <p class="desc">District Manager NC, SC, TN and VA & Marketing Manager at WorldWide Medical Products, Inc</p>
-		      </div>
-		    </td>
-		    
-		    <td>
-		      <!-- Kelly Gewain -->
-		      <div class="personCell">
-			<h3> Lab Technician </h3>
-			<img class="personImg" alt="Kelly Gewain" src="img/kelly.png"/>
-			<p><strong>Kelly Gewain</strong></p>
-			<!--<a href="mailto:rabialexander@gmail.com">rabialexander@gmail.com</a>-->
-			<p class="desc">Currently an MD/PhD student at UNC - Chapel Hill Medical School.</p>
-		      </div>
-		    </td>
-                    <td>
-                      <!-- Bridgid Hast -->
-                      <div class="personCell">
-                        <h3> Graduate Student </h3>
-                        <img class="personImg" alt="Bridgid Hast" src="img/bridgid.png"/>
-                        <p><strong>Bridgid Hast</strong></p>
-                        <!--<a href="mailto:bridgid_hast@med.unc.edu" target="_blank">bridgid_hast@med.unc.edu</a>-->
-                        <p class="desc">Now a medical student at East Carolina University.</p>
-                      </div>
-                    </td>
-                    <td>
-                      <!-- Alex Rabinowitz -->
-                      <div class="personCell">
-                        <h3> Lab Technician </h3>
-                        <img class="personImg" alt="Alex Rabinowitz" src="img/alex.png"/>
-                        <p><strong>Alex Rabinowitz</strong></p>
-                        <a href="mailto:rabialexander@gmail.com" target="_blank">rabialexander@gmail.com</a>
-                        <p class="desc">Lab Technician at the <a href="http://www.med.unc.edu/geneticsdept/asokanlab/Asokan_La_1./Home.html" target="_blank">Asokan Lab.</a></p>
-                      </div>
-                    </td>
-		    
-		  </tr>
-		</tbody>
-	      </table>
-	      
-	    </div>
-	  </div>
+					<div class="content">
+						<div class="sliderContainer">
+							<div class="sliderFrame">
+								<div id="slider" class="slider">
+									<img src="img/lab/lab1.png" alt="" />
+									<img src="img/lab/lab4.png" alt="" />
+									<img src="img/lab/lab3.png" alt="" />
+									<img src="img/lab/lab5.png" alt="" />
+									<img src="img/lab/lab2.png" alt="" />
+								</div>
+							</div>
+						</div>
+
+
+
+						<table class="people" width="100%">
+							<tbody>
+								<tr>
+									<td>
+										<!-- Ben Major -->
+										<div class="personCell">
+											<h3> Principal Investigator </h3>
+											<img class="personImg" alt="Dr. Ben Major" src="img/ben.png"/>
+											<p><strong>Ben Major, PhD <a href="files/ben_biosketch.pdf" target="_blank">(Biosketch)</a></strong></p>
+											<a href="mailto:ben_major@med.unc.edu" target="_blank">ben_major@med.unc.edu</a>
+											<p class="desc"></p>
+										</div>
+									</td>
+									<td>
+										<!-- Priscila Siesser -->
+										<div class="personCell">
+											<h3> Research Associate </h3>
+											<img class="personImg" alt="Priscila Siesser" src="img/priscila.jpg"/>
+											<p><strong>Priscila Siesser, PhD</strong></p>
+											<a href="mailto:priscila_siesser@med.unc.edu" target="_blank">priscila_siesser@med.unc.edu</a>
+											<p class="desc">Cell Biology.<br/>Cytoskeleton and motility.</p>
+										</div>
+									</td>
+									<td>
+										<!-- Matt Walker -->
+										<div class="personCell">
+											<h3> Postdoctoral Fellow </h3>
+											<img class="personImg" alt="Matt Walker" src="img/matt.png"/>
+											<p><strong>Matt Walker, PhD</strong></p>
+											<a href="mailto:mattwa@med.unc.edu" target="_blank">mattwa@med.unc.edu</a>
+											<p class="desc">Development of functional screening platforms. Team Wnt.</p>
+										</div>
+									</td>
+									<td>
+										<!-- Feng Yang -->
+										<div class="personCell">
+											<h3> Postdoctoral Fellow </h3>
+											<img class="personImg" alt="Feng Yang" src="img/feng.png"/>
+											<p><strong>Feng Yang, PhD</strong></p>
+											<a href="mailto:fyan@unc.edu" target="_blank">fyan@unc.edu</a>
+											<p class="desc">Mass spectrometry, activity-based proteomics, and phosphoproteomics</p>
+										</div>
+									</td>
+								</tr>
+
+								<tr>
+									<td>
+										<!-- Dennis Goldfarb -->
+										<div class="personCell">
+											<h3> Graduate Student </h3>
+											<img class="personImg" alt="Dennis Goldfarb" src="img/dennis2.png"/>
+											<p><strong>Dennis Goldfarb</strong></p>
+											<a href="mailto:goldfarb@cs.unc.edu">goldfarb@cs.unc.edu</a>
+											<p class="desc">Co-mentored by <a href="http://www.cs.unc.edu/~weiwang/" target="_blank" style="font-size:12px">Wei Wang</a>
+											</p>
+											<p class="desc">Protein-protein interaction prediction and software development.</p>
+										</div>
+									</td>
+									<td>
+										<!-- Kathleen Mulvaney -->
+										<div class="personCell">
+											<h3> Graduate Student </h3>
+											<img class="personImg" alt="Kathleen Mulvaney" src="img/kathleen.png"/>
+											<p><strong>Kathleen Mulvaney</strong></p>
+											<a href="mailto:kmulvane@email.unc.edu" target="_blank">kmulvane@email.unc.edu</a>
+											<p class="desc">Team Keap/Nrf.</br>Lab safety officer.</p>
+										</div>
+									</td>
+									<td>
+										<!-- Erica Cloer -->
+										<div class="personCell">
+											<h3> Graduate Student </h3>
+											<img class="personImg" alt="Erica Cloer" src="img/erica.png"/>
+											<p>(blurry) <strong>Erica Cloer</strong></p>
+											<a href="mailto:erica_cloer@med.unc.edu" target="_blank">erica_cloer@med.unc.edu</a>
+											<p class="desc">Team Keap/Nrf.</p>
+										</div>
+									</td>
+									<td>
+										<!-- Tigist Y Tamir -->
+										<div class="personCell">
+											<h3> Graduate Student </h3>
+											<img class="personImg" alt="Tigist Y Tamir" src="img/tigist.jpeg"/>
+											<p><strong>Tigist Y Tamir</strong></p>
+											<a href="mailto:tytamir@email.unc.edu" target="_blank">tytamir@email.unc.edu</a>
+											<p class="desc">Team Keap/Nrf.</br>Team Wnt.</p>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<!-- Abby Winn -->
+										<div class="personCell">
+											<h3> Lab Technician </h3>
+											<img class="personImg" alt="Abby Winn" src="img/Abby.png"/>
+											<p><strong>Abby Winn</strong></p>
+											<a href="mailto:arwinn@live.unc.edu" target="_blank">arwinn@live.unc.edu</a>
+											<p class="desc">Support Team.</br>Minion Master.</p>
+										</div>
+									</td>
+									<td>
+										<!-- Joe Dayaa -->
+										<div class="personCell">
+											<h3> Undergraduate </h3>
+											<img class="personImg" alt="Joe Dayaa" src="img/johndoe.png"/>
+											<p><strong>Joe Dayaa</strong></p>
+											<a href="mailto:dayaa@live.unc.edu" target="_blank">dayaa@live.unc.edu</a>
+											<p class="desc">Team Wnt.</p>
+										</div>
+									</td>
+									<td>
+										<!-- Nalini Bernadette Peres-da-silva -->
+										<div class="personCell">
+											<h3> Undergraduate </h3>
+											<img class="personImg" alt="Nalini Bernadette Peres-da-silva" src="img/nalini.png"/>
+											<p><strong>Nalini Bernadette</br>Peres-da-silva</strong></p>
+											<a href="mailto:nalinip@live.unc.edu" target="_blank">nalinip@live.unc.edu</a>
+											<p class="desc">Support Team.</br>Minion.</p>
+										</div>
+									</td>
+
+									<td>
+									</td>
+									
+									<td/>
+									<td/>
+								</tr>
+							</tbody>
+						</table>
+
+
+						<p class="divider-blue">Major Lab Alumni</p>
+
+						<table class="people" style="width:100%; margin-top:15px;">
+							<tbody>
+								<tr>
+									<td>
+										<!-- Kathryn Mackinnon -->
+										<div class="personCell">
+											<h3> Postdoctoral Fellow </h3>
+											<img class="personImg" alt="Kathryn Mackinnon" src="img/kathryn.png"/>
+											<p><strong>Kathryn Mackinnon, PhD</strong></p>
+											<a href="mailto:kmackinnon@wwmponline.com" target="_blank">kmackinnon@wwmponline.com</a>
+											<p class="desc">District Manager NC, SC, TN and VA & Marketing Manager at WorldWide Medical Products, Inc</p>
+										</div>
+									</td>
+
+									<td>
+										<!-- Kelly Gewain -->
+										<div class="personCell">
+											<h3> Lab Technician </h3>
+											<img class="personImg" alt="Kelly Gewain" src="img/kelly.png"/>
+											<p><strong>Kelly Gewain</strong></p>
+											<!--<a href="mailto:rabialexander@gmail.com">rabialexander@gmail.com</a>-->
+											<p class="desc">Currently an MD/PhD student at UNC - Chapel Hill Medical School.</p>
+										</div>
+									</td>
+									<td>
+										<!-- Bridgid Hast -->
+										<div class="personCell">
+											<h3> Graduate Student </h3>
+											<img class="personImg" alt="Bridgid Hast" src="img/bridgid.png"/>
+											<p><strong>Bridgid Hast</strong></p>
+											<!--<a href="mailto:bridgid_hast@med.unc.edu" target="_blank">bridgid_hast@med.unc.edu</a>-->
+											<p class="desc">Now a medical student at East Carolina University.</p>
+										</div>
+									</td>
+									<td>
+										<!-- Alex Rabinowitz -->
+										<div class="personCell">
+											<h3> Lab Technician </h3>
+											<img class="personImg" alt="Alex Rabinowitz" src="img/alex.png"/>
+											<p><strong>Alex Rabinowitz</strong></p>
+											<a href="mailto:rabialexander@gmail.com" target="_blank">rabialexander@gmail.com</a>
+											<p class="desc">Lab Technician at the <a href="http://www.med.unc.edu/geneticsdept/asokanlab/Asokan_La_1./Home.html" target="_blank">Asokan Lab.</a></p>
+										</div>
+									</td>
+
+								</tr>
+							</tbody>
+						</table>
+
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
-      </div>
-    </div>
-    <?php include "footer.html";?> 
-  </body>
+<?php include "footer.html";?> 
+</body>
 </html>
